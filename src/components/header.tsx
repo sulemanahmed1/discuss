6e4a94d5e35366fe0e8d38540fd28afba1bf4570
suleman ahmed <ahmedsuleman@outlook.dk>
2023-12-14 15:30:41 +0100

    auth is moved from  header to header-auth

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,26 @@
 import Link from "next/link";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Input, Button, Avatar } from '@nextui-org/react'; 
-import { auth } from '@/auth'; 
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Input} from '@nextui-org/react';
+import HeaderAuth from "@/components/header-auth"; 
 
-export default async function Header() {
+export default function Header() {
+     
+
     
-    
+   
+
+    return (
+        <Navbar className="shadow mb-6">
+            <NavbarBrand>
+                <Link href="/" className="font-bold">Discuss</Link>
+            </NavbarBrand>
+            <NavbarContent justify="center">
+                <Input />
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <HeaderAuth />
+            </NavbarContent>
+        </Navbar>
+    )
 }
 
 
