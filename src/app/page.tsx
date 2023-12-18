354eb5a97089bff932a819a0a9e6793b96c1974e
suleman ahmed <ahmedsuleman@outlook.dk>
2023-12-18 14:56:35 +0100

    added topics and posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,20 @@
-import { Button } from '@nextui-org/react';
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
+import TopicCreateForm from "@/components/topics/topic-create-form"
+import TopicList from "@/components/topics/topic-list"
+import { Divider } from "@nextui-org/react"
 
-export default async function Home() {
-  const session = await auth(); 
+TopicCreateForm
+export default function Home() {
 
   return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type='submit'>sign In </Button>
-      </form>
-
-      <form action={actions.signOut}>
-        <Button type='submit'>SignOut </Button>
-      </form>
-
-      {
-        session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed out</div>
-      }
-
-      <Profile /> 
+    <div className="grid grid-cols-4  gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div className="border shadow py-3 px-2">
+        <TopicCreateForm />
+        <Divider className="my-2"/>
+        <TopicList />
+      </div>
     </div>
   )
 }
--- src/app/topics/[slug]/posts/new/[postId]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PostShowPage() {
-    return <div>Post Show Page</div>
-}
