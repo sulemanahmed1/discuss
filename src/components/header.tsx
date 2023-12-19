a6564fc298447df51e7492e9e835cc8b7342ac69
suleman ahmed <ahmedsuleman@outlook.dk>
2023-12-19 05:48:14 +0100

    added search functionality

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,20 @@
 import Link from "next/link";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Input} from '@nextui-org/react';
-import HeaderAuth from "@/components/header-auth"; 
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, } from '@nextui-org/react';
+import HeaderAuth from "@/components/header-auth";
+import SearchInput from "./search-input";
+import { Suspense } from "react";
+
 
 export default function Header() {
-     
-
-    
-   
-
     return (
         <Navbar className="shadow mb-6">
             <NavbarBrand>
                 <Link href="/" className="font-bold">Discuss</Link>
             </NavbarBrand>
             <NavbarContent justify="center">
-                <Input />
+                <Suspense>
+                    <SearchInput />
+                </Suspense>
             </NavbarContent>
             <NavbarContent justify="end">
                 <HeaderAuth />
@@ -22,5 +22,3 @@ export default function Header() {
         </Navbar>
     )
 }
-
-
