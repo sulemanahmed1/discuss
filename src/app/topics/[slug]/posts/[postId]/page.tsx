181c127e53f0bfc11910b65c3bad9947a0fbdb1f
suleman ahmed <ahmedsuleman@outlook.dk>
2023-12-19 05:49:18 +0100

    bugfix: wrap PostShow with suspense

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -3,6 +3,9 @@ import PostShow from "@/components/posts/post-show";
 import CommentList from "@/components/comments/comment-list";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
+import { fetchCommentsByPostId } from "@/db/queries/comments";
+import { Suspense } from "react";
+import PostShowLoading from "@/components/posts/post-show-loading";
 
 interface PostShowPageProps {
   params: {
@@ -19,9 +22,12 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"< "}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
-      {/* <CommentCreateForm postId={postId} startOpen /> */}
-      {/* <CommentList comments={comments} /> */}
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
+
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList postId={postId} />
     </div>
   );
 }
